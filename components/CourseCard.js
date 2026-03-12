70ac9a6d58c3caa52dbbe2291796cdf02f83971e
elhadrisa <elhadri.sa@gmail.com>
2026-03-12 17:48:27 +0100

    animation matieres

--- components/CourseCard.js
+++ components/CourseCard.js
@@ -1,12 +1,12 @@
+import NetworkAnimation from "./NetworkAnimation"
 export default function CourseCard({ title, pdf }) {
 
   return (
 
     <div className="border border-green-400 p-6 hover:bg-green-400 hover:text-black transition">
-
-      <div className="h-24 mb-4 flex items-center justify-center border border-green-400">
-        animation réseau
-      </div>
+<div className="h-24 mb-4 flex items-center justify-center border border-green-400">
+  <NetworkAnimation />
+</div>
 
       <h2 className="text-xl mb-3 neon-text">
         {title}
