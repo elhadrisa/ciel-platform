1d932ff62e4ccedcaa0a10600087a694a019139f
elhadrisa <elhadri.sa@gmail.com>
2026-03-12 12:39:17 +0100

    add hacker font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Orbitron } from 'next/font/google'
+import './globals.css'
+
+const orbitron = Orbitron({
+  subsets: ['latin'],
+})
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,9 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+     <body className={orbitron.className}>
         {children}
       </body>
     </html>
